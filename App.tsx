e55125b28749578db5780c6e2a123bd4a9f4329b
Michelsom <michelsonpagani@gmail.com>
2022-03-24 14:18:27 -0400

    chore:new libs added

--- App.tsx
+++ App.tsx
@@ -3,18 +3,34 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./src/Routes/app.routes";
 import { AppProvider } from "./src/context/app";
-
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components/native";
 import theme from "./src/global/styles/theme";
+import * as Roboto from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
+import { Platform, View } from "react-native";
 
-export default function App() {
+export const App = () => {
+  const [fontsLoaded] = Roboto.useFonts({
+    Roboto_100Thin: Roboto.Roboto_100Thin,
+    Roboto_300Light: Roboto.Roboto_300Light,
+    Roboto_400Regular: Roboto.Roboto_400Regular,
+    Roboto_500Medium: Roboto.Roboto_500Medium,
+    Roboto_700Bold: Roboto.Roboto_700Bold,
+    Roboto_900Black: Roboto.Roboto_900Black,
+   
+  });
+  if (!fontsLoaded) {
+    if (Platform.OS === "ios")
+      return <View style={{ backgroundColor: "", flex: 1 }} />;
+    return <AppLoading />;
+  }
   return (
-    <AppProvider>
-      <NavigationContainer>
-        <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <AppProvider>
+        <NavigationContainer>
           <AppRoutes />
-        </ThemeProvider>
-      </NavigationContainer>
-    </AppProvider>
+        </NavigationContainer>
+      </AppProvider>
+    </ThemeProvider>
   );
-}
+};
