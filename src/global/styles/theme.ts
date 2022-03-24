ea0d10178988d6f0c31efbba6653b46a51199106
Michelsom <michelsonpagani@gmail.com>
2022-03-24 14:15:10 -0400

    design:new global created

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,5 +1,18 @@
 export default {
   colors: {
-    
+    lightColor: "#FFFFFF",
+    primaryColor: "#2ab598",
+    secondaryColor: "#FDA506",
+    descriptionTextColor: "#868686",
+    borderColor: "#0000000F",
+    TitleText:"#535353",
+  },
+  fonts: {
+    robotoThin: "Roboto_100Thin",
+    robotoLight: "Roboto_300Light",
+    robotoRegular: "Roboto_400Regular",
+    robotoMedium: "Roboto_500Medium",
+    robotoBold: "Roboto_700Bold",
+    robotoBlack: "Roboto_900Black",
   },
 };
