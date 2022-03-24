2d43b32a8b41312c07869ff8fe3702bbf8fbaf40
Michelsom <michelsonpagani@gmail.com>
2022-03-24 14:15:21 -0400

    models:new model created

---
+++ src/models/specificPost.ts
@@ -0,0 +1,111 @@
+export interface SpecificPostInterface {
+  id: number;
+  date: string;
+  date_gmt: string;
+  guid: {
+    rendered: string;
+  };
+  modified: string;
+  modified_gmt: string;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+    protected: boolean;
+  };
+  excerpt: {
+    rendered: string;
+    protected: boolean;
+  };
+  author: number;
+  featured_media: number;
+  comment_status: string;
+  ping_status: string;
+  sticky: boolean;
+  template: string;
+  format: string;
+  meta: {
+    amp_status: string;
+  };
+  categories: number[];
+
+  tags: number[];
+
+  page_views: number;
+  _links: {
+    self: [
+      {
+        href: string;
+      }
+    ];
+    collection: [
+      {
+        href: string;
+      }
+    ];
+    about: [
+      {
+        href: string;
+      }
+    ];
+    author: [
+      {
+        embeddable: boolean;
+        href: string;
+      }
+    ];
+    replies: [
+      {
+        embeddable: boolean;
+        href: string;
+      }
+    ];
+    versionHistory: [
+      {
+        count: number;
+        href: string;
+      }
+    ];
+    predecessorVersion: [
+      {
+        id: number;
+        href: string;
+      }
+    ];
+    wpFeaturedmedia: [
+      {
+        embeddable: boolean;
+        href: string;
+      }
+    ];
+    wpAttachment: [
+      {
+        href: string;
+      }
+    ];
+    wpTerm: [
+      {
+        taxonomy: string;
+        embeddable: boolean;
+        href: string;
+      },
+      {
+        taxonomy: string;
+        embeddable: boolean;
+        href: string;
+      }
+    ];
+    curies: [
+      {
+        name: string;
+        href: string;
+        templated: boolean;
+      }
+    ];
+  };
+}
